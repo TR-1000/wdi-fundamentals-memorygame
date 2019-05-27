416c26cc7fff095349679a0a6fc357686ed19963
TR-1000 <t.l.ross@outlook.com>
2019-05-26 22:34:48 -0500

    Created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+window.alert("Up and running!");
+var cardOne = "King";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "Queen";
+
+console.log("Player flipped "+cardFour)
+console.log("Player flipped "+cardOne)
