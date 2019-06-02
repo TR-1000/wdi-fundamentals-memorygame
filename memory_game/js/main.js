c6828097728509d76ca5ef57bfe5ed27aa1247c5
TR-1000 <t.l.ross@outlook.com>
2019-06-01 22:48:30 -0500

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,31 +1,26 @@
-window.alert("JS up and running!");
+//window.alert("JS up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-//cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-//cardsInPlay.push(cardTwo);
-
-var cardThree = cards[1];
-cardsInPlay.push(cardThree);
-
-var cardFour = cards[3];
-//cardsInPlay.push(cardFour);
-
-//console.log("Player flipped "+cardOne)
-//console.log("Player flipped "+cardTwo)
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch(){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+			console.log("Sorry, try again.");
+		}
 	}
 }
-else {
-	alert("Please pick 2 cards.")
+
+
+function flipCard(cardId){
+	console.log("Player flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 }
 
+
+flipCard(0);
+flipCard(2);
