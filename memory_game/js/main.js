40dba27db80faa1afdb72d40d14e925df5f7a88d
TR-1000 <t.l.ross@outlook.com>
2019-06-04 21:34:04 -0500

    Created card objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,32 @@
 //window.alert("JS up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "image/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "image/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "image/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	image: "image/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
-function checkForMatch(){
-	if (cardsInPlay.length === 2){
-		if (cardsInPlay[0] === cardsInPlay[1]){
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 		}
 		else {
@@ -15,9 +37,11 @@ function checkForMatch(){
 }
 
 
-function flipCard(cardId){
-	console.log("Player flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+function flipCard(cardId) {
+	console.log("Player flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 
